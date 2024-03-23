d3491c642fb0a7f9a4dea56dfe3a71d5ff29b403
Thiago Rosa <thiagosrosa@gmail.com>
2024-03-23 01:29:49 -0300

    feat: use loaders

--- backend/src/modules/prisma/prisma.service.ts
+++ backend/src/modules/prisma/prisma.service.ts
@@ -1,12 +1,40 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { loader_concept_relationship_2b } from './loader/loader';
+import { loader_2b_concept_relationship } from './loaders/loader_2b_concept_relationship';
+import { loader_2b_concept } from './loaders/loader_2b_concept';
+import { loader_2b_vocabulary } from './loaders/loader_2b_vocabulary';
+import { loader_care_site } from './loaders/loader_care_site';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  async onModuleInit() {
-    await this.$connect();
+  constructor() {
+    super();
+  }
 
-    loader_concept_relationship_2b({ prismaService: this });
+  private readonly logger = new Logger('DrGaia: ');
+
+  async onModuleInit() {
+    try {
+      await this.$connect();
+      this.logger.log('Connected to database successfully.');
+
+      // Adding a delay of 10 seconds
+      this.logger.log('Loading database tables. This may take a while...');
+      await this.delay(10000);
+
+      this.logger.log('Starting data loading process.');
+
+      loader_2b_concept_relationship({ prismaService: this });
+      loader_2b_concept({ prismaService: this });
+      loader_2b_vocabulary({ prismaService: this });
+      //loader not needed for attribute_definition.csv - no data
+      loader_care_site({ prismaService: this });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  private delay(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
 }
