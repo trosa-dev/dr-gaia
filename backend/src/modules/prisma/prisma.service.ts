0dce8cae9b2222d9253691c4015e0dbafa6a1d98
Thiago Rosa <thiagosrosa@gmail.com>
2024-03-23 23:16:36 -0300

    feat: use loader_cdm_source

--- backend/src/modules/prisma/prisma.service.ts
+++ backend/src/modules/prisma/prisma.service.ts
@@ -4,6 +4,7 @@ import { loader_2b_concept_relationship } from './loaders/loader_2b_concept_rela
 import { loader_2b_concept } from './loaders/loader_2b_concept';
 import { loader_2b_vocabulary } from './loaders/loader_2b_vocabulary';
 import { loader_care_site } from './loaders/loader_care_site';
+import { loader_cdm_source } from './loaders/loader_cdm_source';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
@@ -29,6 +30,7 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
       loader_2b_vocabulary({ prismaService: this });
       //loader not needed for attribute_definition.csv - no data
       loader_care_site({ prismaService: this });
+      loader_cdm_source({ prismaService: this });
     } catch (error) {
       console.log(error);
     }
