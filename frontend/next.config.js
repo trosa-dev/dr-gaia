b5167680557b3af5e7ea3a4f97c7724506b1c89e
Thiago Rosa <thiagosrosa@gmail.com>
2024-03-29 22:29:15 -0300

    refactor: remove frontend

--- frontend/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: process.env.BASEPATH,
-  
-  // TODO: below line is added to resolve twice event dispatch in the calendar reducer
-  reactStrictMode: false
-}
-
-module.exports = nextConfig
